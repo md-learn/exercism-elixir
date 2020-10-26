a192807504fc69cd79014bc3b07b4dca827b3160
Marco Denisi <marcodenisi35@gmail.com>
2020-10-26 16:51:14 +0100

    feat(protein-translation): solved protein translation

---
+++ protein-translation/lib/protein_translation.ex
@@ -0,0 +1,67 @@
+defmodule ProteinTranslation do
+
+  @codons %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    rna
+    |> String.codepoints()
+    |> Enum.chunk_every(3)
+    |> Enum.map(&Enum.join/1)
+    |> Enum.reduce_while({:ok, []}, &reduce(of_codon(&1), &2))
+  end
+
+  defp reduce({:ok, protein}, acc) when protein != "STOP", do: {:cont, {:ok, elem(acc, 1) ++ [protein]}}
+  defp reduce({:ok, _}, acc), do: {:halt, acc}
+  defp reduce({:error, _}, _), do: {:halt, {:error, "invalid RNA"}}
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    case Map.fetch(@codons, codon) do
+      :error -> {:error, "invalid codon"}
+      res -> res
+    end
+  end
+end
