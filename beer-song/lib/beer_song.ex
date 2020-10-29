3b15aa3a06883bac3ef0901e2cd45d18ec183571
Marco Denisi <marcodenisi35@gmail.com>
2020-10-29 18:25:47 +0100

    feat(beer-song): remove useless number variable

--- beer-song/lib/beer_song.ex
+++ beer-song/lib/beer_song.ex
@@ -7,16 +7,16 @@ defmodule BeerSong do
     do_verse(number)
   end
 
-  defp do_verse(number = 2) do
+  defp do_verse(2) do
     """
-    #{number} bottles of beer on the wall, #{number} bottles of beer.
-    Take one down and pass it around, #{number - 1} bottle of beer on the wall.
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
     """
   end
 
-  defp do_verse(number = 1) do
+  defp do_verse(1) do
     """
-    #{number} bottle of beer on the wall, #{number} bottle of beer.
+    1 bottle of beer on the wall, 1 bottle of beer.
     Take it down and pass it around, no more bottles of beer on the wall.
     """
   end
